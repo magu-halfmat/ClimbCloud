6e9f50ba1590f4ee0dc7964fbb0154b769a603cd
magu-halfmat <gamemnhr@gmail.com>
2023-04-22 23:39:04 +0900

    PlayerControllerつけた

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            key = 1;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            key = -1;
+        }
+
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        if (key != 0)
+        {
+            this.transform.localScale = new Vector3(key, 1, 1);
+        }
+
+    }
+}
